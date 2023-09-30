594778c5a9297dd6f826e483444c7a05a0e81c69
0xturboblitz <florent.tavernier@gmail.com>
2023-09-30 10:50:18 +0200

    calling openAI api

--- utils.py
+++ utils.py
@@ -1,7 +1,37 @@
-def api_call(text):
-    return "", ""
+import openai
+import os
+from dotenv import load_dotenv
+import re
 
+load_dotenv()
 
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+async def api_call(prompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": prompt
+            },
+        ]
+    )
+
+    response = completion.choices[0].message.get("content", "")
+
+    # print("response", response)
+
+    answer_match = re.search(r'ANSWER: "(.*?)"', response)
+    thought_match = re.search(r'THOUGHT: "(.*?)"', response)
+    
+    answer = answer_match.group(1) if answer_match else None
+    thought = thought_match.group(1) if thought_match else None
+    
+    # print("Answer:", answer)
+    # print("Thought:", thought)
+    
+    return answer, thought
 
 def extract_vote(text):
     return "Paul"
